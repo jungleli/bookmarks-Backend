2f2ae5968b2d863f37fa40ec0b1d3b938052809c
jungleli <jungle216@126.com>
2015-12-10 23:48:39 +0800

    Module: Delete node-modules

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,13 +2,14 @@ $(function () {
 
     var page = 1;
     var rows = 10;
-    loadData(page, rows);
+    var phrase = "";
+    loadData(page, rows, phrase);
 
     function loadData(page, rows, phrase) {
         var pageData = {
-            'page': page,
-            'rows': rows,
-            'key': phrase
+            'page': page || 1,
+            'rows': rows || 10,
+            'key': phrase || ''
         }
         $.ajax({
             type: 'get',
@@ -16,34 +17,30 @@ $(function () {
             url: '../query',
             dataType: 'JSON'
         }).done(function (data) {
+            var bookmarks = _.chain(data.items)
+                .map(function (val, key) {
+                    return {
+                        id: val._id, title: val.title,
+                        url: val.url, created: formatDate(val.created)
+                    };
+                })
+                .sortBy("created")
+                .reverse()
+                .value();
+            var totalItem = data.total;
+            var totalPage = Math.ceil(totalItem / rows) || 1;
 
-                var regExp = new RegExp((phrase), "gi");
-                // if (data.msg === '') {
-                var bookmarks = _.chain(data.items)
-                    .map(function (val, key) {
-                        return {
-                            id: val._id, title: val.title,
-                            url: val.url, created: formatDate(val.created)
-                        };
-                    })
-                    .sortBy("created")
-                    .reverse()
-                    .value();
-                var totalItem = data.total;
-                var totalPage = Math.ceil(totalItem / rows) || 1;
-
-                updateList(bookmarks, totalItem);
-                updatePageNav(totalItem, totalPage, page);
-                $("#now").html(page + "/" + totalPage);
-                $("#total").html(totalItem);
-                page == 1 ? $("#pre").hide() : $("#pre").show();
-                page == totalPage ? $("#next").hide() : $("#next").show()
-
-                $(".list *").highlight(regExp, "highlight");
-
-            });
+            updateList(bookmarks, totalItem);
+            updatePageNav(totalItem, totalPage, page);
+            $("#now").html(page + "/" + totalPage);
+            $("#total").html(totalItem);
+            page == 1 ? $("#pre").hide() : $("#pre").show();
+            page == totalPage ? $("#next").hide() : $("#next").show();
+            if (phrase !== '') {
+                $(".list *").highlight(phrase, "highlight");
+            }
+        });
     };
-
 
     $("#add").on("click", addDialog);
     $("#close").on("click", addDialog)
@@ -51,17 +48,15 @@ $(function () {
         var display = $("#addDialog").css("visibility");
         $("#addDialog").css("visibility",
             display == "visible" ? "hidden" : "visible");
-
     }
 
-    $('#AddBookmark').on('click', function(){
+    $('#AddBookmark').on('click', function () {
         var errorCount = 0;
         $('#addDialog input').each(function (index, val) {
             if ($(this).val() === '') {
                 errorCount++;
             }
         });
-
         if (errorCount === 0) {
             var newItem = {
                 'title': $('#inputTitle').val(),
@@ -81,8 +76,8 @@ $(function () {
                 else {
                     alert('Error: ' + response.msg);
                 }
-                loadData(page, rows);
-                $('#addDialog input').val('');
+                loadData(page, rows, $("#search").val().trim());
+                $('#addDialog input[type=text]').val('');
                 $('#close').click();
             });
         }
@@ -92,47 +87,8 @@ $(function () {
         }
     });
 
-    // function addBookmark() {
-    //     var errorCount = 0;
-    //     $('#addDialog input').each(function (index, val) {
-    //         if ($(this).val() === '') {
-    //             errorCount++;
-    //         }
-    //     });
-
-    //     if (errorCount === 0) {
-    //         var newItem = {
-    //             'title': $('#inputTitle').val(),
-    //             'created': Date.parse(new Date()) / 1000,
-    //             'url': $('#inputURL').val()
-    //         }
-    //         $.ajax({
-    //             type: 'POST',
-    //             data: newItem,
-    //             url: '../add',
-    //             dataType: 'JSON'
-    //         }).done(function (response) {
-    //             if (response.status === 'success') {
-    //                 console.dir(response);
-    //                 alert("success: " + response.msg);
-    //             }
-    //             else {
-    //                 alert('Error: ' + response.msg);
-    //             }
-    //             loadData(page, rows);
-    //             $('#addDialog input').val('');
-    //             $('#close').click();
-    //         });
-    //     }
-    //     else {
-    //         alert("请输入正确的书签名称/书签链接");
-    //         return false;
-    //     }
-    // };
-
-
-    $('.list').on('click', 'li a.delete', function(){
-         if (confirm("确定删除？")) {
+    $('.list').on('click', 'li a.delete', function () {
+        if (confirm("确定删除？")) {
             $.ajax({
                 type: 'DELETE',
                 url: '../delete/' + $(this).attr('rel')
@@ -144,7 +100,7 @@ $(function () {
                 else {
                     alert('Fail: ' + response.msg);
                 }
-                loadData(page, rows);
+                loadData(page, rows,$("#search").val().trim());
             });
         }
         else {
@@ -152,43 +108,13 @@ $(function () {
         }
     });
 
-    // function deleteBookmark() {
-    //     if (confirm("确定删除？")) {
-    //         $.ajax({
-    //             type: 'DELETE',
-    //             url: '../delete/' + $(this).attr('rel')
-    //         }).done(function (response) {
-    //             if (response.status === 'success') {
-    //                 console.dir(response);
-    //                 alert("success: " + response.msg);
-    //             }
-    //             else {
-    //                 alert('Fail: ' + response.msg);
-    //             }
-    //             loadData(page, rows);
-    //         });
-    //     }
-    //     else {
-    //         return false;
-    //     }
-    // };
-
-
-    $('#next').on('click', function(){
+    $('#next').on('click', function () {
         loadData(++page, rows, $("#search").val().trim());
     });
 
-    $('#pre').on('click', function(){
+    $('#pre').on('click', function () {
         loadData(--page, rows, $("#search").val().trim());
     });
-
-    //function pre() {
-    //    loadData(--page, rows, $("#search").val().trim());
-    //}
-    //
-    //function next() {
-    //    loadData(++page, rows, $("#search").val().trim());
-    //}
 
     $("#search").on('input onchange', function () {
         loadData(page, rows, $(this).val().trim());
@@ -200,7 +126,6 @@ $(function () {
             "bookmarks": bookmarks,
             'totalItem': total, 'page': 1
         });
-
         $(".list").html(html);
     }
 
@@ -208,12 +133,12 @@ $(function () {
         var compiled = _.template($("#pageNav").html());
         var html = compiled({'totalItem': totalItem, 'totalPage': totalPage});
         $('.pageNav').html(html);
-
         $('.pageNav li').on('click', function () {
             page = $(this).html();
             loadData(page, rows, $("#search").val().trim());
         });
     }
+
     function formatDate(date) {
         var time = new Date(parseInt(date) * 1000);
         var year = time.getFullYear();
@@ -223,15 +148,17 @@ $(function () {
         day = day > 10 ? day : '0' + day;
         return year + '-' + month + '-' + day;
     }
-    jQuery.fn.highlight = function (regex, className) {
+
+    jQuery.fn.highlight = function (phrase, className) {
+        var regExp = new RegExp((phrase), "gi");
         return this.each(function () {
             $(this).contents()
                 .filter(function () {
-                    return this.nodeType == 3 && regex.test(this.nodeValue);
+                    return this.nodeType == 3 && regExp.test(this.nodeValue);
                 })
                 .replaceWith(function () {
                     return (this.nodeValue || "")
-                        .replace(regex, function (match) {
+                        .replace(regExp, function (match) {
                             return "<span class=\"" + className + "\">" + match + "</span>";
                         });
                 });
